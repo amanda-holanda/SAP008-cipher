4c5e1c3d7339bbeeb0b2c96822fec206028862aa
Ahda <amandaholanda_@hotmail.com>
2022-07-26 18:30:05 -0300

    atualição do readme e implementação do HTML, CSS e JS

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
-const cipher = {
-  // ...
+const cipher = { encode, decode };
+
+function encode(offset, message){
+
+  let converterTexto = "";
+
+  for (let i = 0, j = message.length; i < j; i++) {
+    let numAscii = ((message.charCodeat(i) - 65 + offset) % 26) + 65;
+    converterTexto += message.fromCharcode(numAscii);    
+  }
+  
+  return converterTexto;
+
+};
+
+function decode(offset, message) {
+  let converterTexto = "";
+  for (let i = 0, j = message.length; i < j; i++) {
+    let numAscii = ((message.charCodeat(i) - 65 + offset) % 26) + 65;
+    converterTexto += message.fromCharcode(numAscii);    
+  }
+  return converterTexto;
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const cifrar = document.querySelector('#btnEncode')
+
+cifrar.addEventListener("click", function(e) {
+    e.preventDefault();
+    console.log("oiola");
+
+    let offset = parseInt(document.querySelector('#offset').value) ;
+
+    let message = document.querySelector('#message').value ;
+    message = message.toUpperCase();
+
+    let result = cipher.encode(offset, message);
+    document.getElementById('cipherMessage').innerHTML = result;
+
+    //console.log(result);
+});
+
+const decifrar = document.querySelector('#btnDecode');
+
+decifrar.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    let offset = parseInt(document.querySelector('#offset').value) ;
+    let message = document.querySelector('#message').value ;
+    
+    let result = cipher.decode(offset, message);
+    document.getElementById('cipherMessage').innerHTML = result;
+
+    //console.log(result);
+});
+
+
